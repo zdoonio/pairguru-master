23bda8c7bd9b71cbb0e9b46fc64b053565341e75
Dominik Zduńczyk <dominik.zdunczyk@goodsoft.pl>
2018-09-20 12:21:57 +0200

    fixed validators

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -9,6 +9,7 @@ class TitleBracketsValidator < ActiveModel::Validator
   private
   def check_title(title)
     bracket_counter = 0
+    previous_character = ""
 
     title.each_char do |char|
       case char
@@ -26,7 +27,10 @@ class TitleBracketsValidator < ActiveModel::Validator
           bracket_counter -= 1
       end
 
+      return false if previous_character =="(" && char == ")"
       return false if bracket_counter < 0
+
+      previous_character = char
     end
 
     return bracket_counter == 0
